0ec5898f3f1337bb1c257836a87d59c617fb4f2b
Kyle McClellan <kmcclellan@overdrive.com>
2022-10-02 20:01:04 -0400

    FIXUP: LogEntryActions

--- Logging.Policies/LogEntryActions.cs
+++ Logging.Policies/LogEntryActions.cs
@@ -2,13 +2,13 @@ namespace Microsoft.Extensions.Logging.Policies;
 
 class LogEntryActions<TEntry, TState>
 {
-    Action<TEntry, string, LogLevel, EventId>? OnEntry { get; set; }
+    public Action<TEntry, string, LogLevel, EventId>? OnEntry { get; set; }
 
-    Action<TEntry, string>? OnMessage { get; set; }
+    public Action<TEntry, string>? OnMessage { get; set; }
 
-    Action<TEntry, Exception>? OnException { get; set; }
+    public Action<TEntry, Exception>? OnException { get; set; }
 
-    Action<TEntry, TState>? OnState { get; set; }
+    public Action<TEntry, TState>? OnState { get; set; }
 
-    LogEntryFilter? Filter { get; set; }
+    public LogEntryFilter? Filter { get; set; }
 }
